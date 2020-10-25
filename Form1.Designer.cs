53290bece1d9b261d0ec6d7d0963f8b024e2b500
Ivan Falchuk <vanya288@gmail.com>
2020-10-25 15:59:39 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exportToTXTBtn = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.loadFromXMLBtn = new System.Windows.Forms.Button();
+            this.openXMLFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openTXTFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.exportToXMLBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridView
+            // 
+            this.gridView.AllowUserToAddRows = false;
+            this.gridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Location = new System.Drawing.Point(12, 86);
+            this.gridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gridView.Name = "gridView";
+            this.gridView.RowHeadersWidth = 51;
+            this.gridView.RowTemplate.Height = 24;
+            this.gridView.Size = new System.Drawing.Size(1287, 250);
+            this.gridView.TabIndex = 0;
+            this.gridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load from text file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.loadFromTXTBtn_Click);
+            // 
+            // exportToTXTBtn
+            // 
+            this.exportToTXTBtn.Location = new System.Drawing.Point(298, 30);
+            this.exportToTXTBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.exportToTXTBtn.Name = "exportToTXTBtn";
+            this.exportToTXTBtn.Size = new System.Drawing.Size(128, 41);
+            this.exportToTXTBtn.TabIndex = 2;
+            this.exportToTXTBtn.Text = "Export to text file";
+            this.exportToTXTBtn.UseVisualStyleBackColor = true;
+            this.exportToTXTBtn.Click += new System.EventHandler(this.exportToTXTBtn_Click);
+            // 
+            // loadFromXMLBtn
+            // 
+            this.loadFromXMLBtn.Location = new System.Drawing.Point(148, 30);
+            this.loadFromXMLBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.loadFromXMLBtn.Name = "loadFromXMLBtn";
+            this.loadFromXMLBtn.Size = new System.Drawing.Size(144, 41);
+            this.loadFromXMLBtn.TabIndex = 3;
+            this.loadFromXMLBtn.Text = "Load from XML file";
+            this.loadFromXMLBtn.UseVisualStyleBackColor = true;
+            this.loadFromXMLBtn.Click += new System.EventHandler(this.loadFromXMLBtn_Click);
+            // 
+            // exportToXMLBtn
+            // 
+            this.exportToXMLBtn.Location = new System.Drawing.Point(432, 30);
+            this.exportToXMLBtn.Name = "exportToXMLBtn";
+            this.exportToXMLBtn.Size = new System.Drawing.Size(134, 41);
+            this.exportToXMLBtn.TabIndex = 4;
+            this.exportToXMLBtn.Text = "Export to XML file";
+            this.exportToXMLBtn.UseVisualStyleBackColor = true;
+            this.exportToXMLBtn.Click += new System.EventHandler(this.exportToXMLBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1311, 492);
+            this.Controls.Add(this.exportToXMLBtn);
+            this.Controls.Add(this.loadFromXMLBtn);
+            this.Controls.Add(this.exportToTXTBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gridView);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Integracja systemów - Ivan Falchuk";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button exportToTXTBtn;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button loadFromXMLBtn;
+        private System.Windows.Forms.OpenFileDialog openXMLFileDialog;
+        private System.Windows.Forms.OpenFileDialog openTXTFileDialog;
+        private System.Windows.Forms.Button exportToXMLBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            openTXTFileDialog.Title = "Browse text file";
+            openTXTFileDialog.FileName = "";
+            openTXTFileDialog.Filter = "TXT files (*.txt)|*.txt";
+            openTXTFileDialog.ShowHelp = true;
+
+            openXMLFileDialog.Title = "Browse xml file";
+            openXMLFileDialog.FileName = "";
+            openXMLFileDialog.Filter = "XML files (*.xml)|*.xml";
+            openXMLFileDialog.ShowHelp = true;
+
+            folderBrowserDialog1.SelectedPath = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void loadFromTXTBtn_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamReader file;
+            if (openTXTFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            file = new System.IO.StreamReader(openTXTFileDialog.FileName);
+
+            string[] columnnames = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16" };
+
+            DataTable dt = new DataTable();
+
+            foreach (string c in columnnames)
+            {
+                dt.Columns.Add(c);
+            }
+
+            string newline;
+
+            while ((newline = file.ReadLine()) != null)
+            {
+                DataRow dr = dt.NewRow();
+                string[] values = newline.Split(';');
+                for (int i = 0; i < values.Length; i++)
+                {
+                    dr[i] = values[i];
+                }
+                dt.Rows.Add(dr);
+            }
+            file.Close();
+            gridView.DataSource = dt;
+        }
+
+        private void loadFromXMLBtn_Click(object sender, EventArgs e)
+        {
+            if (openXMLFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            DataSet dataSet = new DataSet();
+            DataTable dataTable = new DataTable();
+
+            dataSet.ReadXml(openXMLFileDialog.FileName);
+
+            for (int col = 0; col < dataSet.Tables.Count; col++)
+            {
+                dataTable.Merge(dataSet.Tables[col]);
+            }
+
+            gridView.DataSource = dataTable;
+        }
+
+        private void exportToTXTBtn_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+
+            openTXTFileDialog.CheckFileExists = false;
+
+            if (openTXTFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            fileName = folderBrowserDialog1.SelectedPath;
+
+            List<string> list = new List<string>();
+
+            for (int rows = 0; rows < gridView.Rows.Count; rows++)
+            {
+                for (int col = 0; col < gridView.Rows[rows].Cells.Count; col++)
+                {
+                    string value = "";
+
+                    if (gridView.Rows[rows].Cells[col].Value != null)
+                    {
+                        value = gridView.Rows[rows].Cells[col].Value.ToString();
+                    }
+
+                    list.Add(value);
+                }
+                list.Add("\n");
+            }
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(fileName);
+
+            foreach (var item in list)
+            {
+                file.Write(item + (item == "\n" ? "" : ";"));
+            }
+
+            file.Close();
+        }
+
+        private void exportToXMLBtn_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            System.IO.StreamWriter file;
+
+            openXMLFileDialog.CheckFileExists = false;
+
+            if (openXMLFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            file = new System.IO.StreamWriter(openXMLFileDialog.FileName);
+
+            foreach (DataGridViewColumn col in gridView.Columns)
+            {
+                dt.Columns.Add(col.DataPropertyName, col.ValueType);
+            }
+
+            foreach (DataGridViewRow row in gridView.Rows)
+            {
+                DataRow row1 = dt.NewRow();
+
+                for (int i = 0; i < gridView.ColumnCount; i++)
+
+                    row1[i] = (row.Cells[i].Value == null ? DBNull.Value : row.Cells[i].Value);
+
+                dt.Rows.Add(row1);
+            }
+
+            ds.Tables.Add(dt);
+            ds.WriteXml(file);
+
+            this.Close();
+        }
+    }
+}
